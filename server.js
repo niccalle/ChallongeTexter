519d48d7b403f82ce3d8a711140dc0cb0fecf935
Victor <victorc1165@yahoo.com>
2016-02-01 22:35:34 -0800

    Added interval

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var interval = 10000; //  10 seconds
+setInterval(function() {
 request.get({url: 'https://api.challonge.com/v1/tournaments/NicGuacTest/matches.json'},
 function(err, response, body){
     data = JSON.parse(body);
@@ -9,3 +11,6 @@ function(err, response, body){
     });
 })
 .auth('niccalle', 'kybqKzS7sTjMiLi6MZCYGCJR5sgQZEczlI747hPR', true);
+}, interval);
+
+/* .auth('chenboy3', 'DTQjITLqFKyJR8kAGh8oK1uS0VBaPXjymwC2iL3d', true); */
