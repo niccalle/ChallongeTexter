ac992542fc7015c11755c66838e6f3e514b42a4d
Nicolas Calle <nicolas.calle@gmail.com>
2016-02-04 20:57:00 -0800

    Added some front end maybe idk

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('challongeApp', []);
+
+app.controller('ChallongeController', function($scope, $http){
+
+});
--- server.js
+++ server.js
@@ -1,91 +1,14 @@
-//user must install "ReadLine Sync" with command - npm install readline-sync
-//install twillio = npm install -g twilio
-var twilio = require('twilio');
-var client = require('twilio')('', '');
-var request = require('request');
-var interval = 2000; //  10 seconds
-var players = {}
-var numbers = {}
-var matchesPinged = {}
-var readlineSync = require('readline-sync');
 
 
+var express = require('express');
+var app = express();
+var config = require('./server/config');
+app = config(app);
 
-//Get Players
-request.get({url: 'https://api.challonge.com/v1/tournaments/NicGuacTest/participants.json'},
-function(err, response, body){
+app.listen(3000, function(){
+    console.log("Server listening at port 3000");
+});
 
-   
-	data = JSON.parse(body);
-	data.forEach(function(element){
-		players[element['participant']['id']] = element['participant']['name'];
-	});
-	for(var key in players){
-		console.log(players[key]);
-        //Gets users number.
-        var userName = readlineSync.question('What is the number of '+ players[key]+'? :');
-        numbers[players[key]]=userName;
-        console.log('Hi ' + players[key] + '! Your number is ' + numbers[players[key]] );
-	}
-	
-})
-.auth('niccalle', 'kybqKzS7sTjMiLi6MZCYGCJR5sgQZEczlI747hPR', true);
-
-//require("path/to/twilio-node/lib");
-// Twilio Credentials 
-
-
-
-
-//require the Twilio module and create a REST client 
-
-
-setInterval(function() {
-request.get({url: 'https://api.challonge.com/v1/tournaments/NicGuacTest/matches.json'},
-function(err, response, body){
-    data = JSON.parse(body);
-    data.forEach(function(element){
-        if(element['match']['state'] == 'open'){
-       		if(matchesPinged[element['match']['id']] != true)
-       		{
-        	console.log(players[element['match']['player1_id']]+" has a match vs "+players[element['match']['player2_id']]);
-            console.log("Sending a text to " + players[element['match']['player1_id']]+ " at number " + numbers[players[element['match']['player1_id']]] +
-            " & " +  players[element['match']['player2_id']]+ " at number " + numbers[players[element['match']['player2_id']]] );
-        	matchesPinged[element['match']['id']] = true;
-        	
-            //send text to players
-                client.sendMessage({
-                    body: "Hello "+ players[element['match']['player1_id']] + "you have a match vs " + players[element['match']['player2_id']],
-                    to: "+1"+numbers[players[element['match']['player1_id']]],
-                    from: "+19256607127"
-                }, function(err, data) {
-                    if(err)
-                        console.log(err);
-                    console.log(data);
-                });
-                client.sendMessage({
-                    body: "Hello "+ players[element['match']['player2_id']] + "you have a match vs " + players[element['match']['player1_id']],
-                    to: "+1"+numbers[players[element['match']['player2_id']]],
-                    from: "+19256607127"
-                }, function(err, data) {
-                    if(err)
-                        console.log(err);
-                    console.log(data);
-                });
-
-
-
-
-            }
-        	console.log(element['match']['player1_id']);
-            console.log(element['match']['id']);
-        }
-    });
-})
-.auth('niccalle', 'kybqKzS7sTjMiLi6MZCYGCJR5sgQZEczlI747hPR', true);
-}, interval);
-
-/* .auth('chenboy3', 'DTQjITLqFKyJR8kAGh8oK1uS0VBaPXjymwC2iL3d', true); */
 
 
 
