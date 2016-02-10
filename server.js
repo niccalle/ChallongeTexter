38dddc528a490c902ad51d360b8dc4aebf493972
Nicolas Calle <nicolas.calle@gmail.com>
2016-02-10 15:28:33 -0800

    Reformatted Code

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-
-
 var express = require('express');
 var app = express();
 var config = require('./server/config');
-app = config(app);
+app = config.initialize(app);
 
 app.listen(3000, function(){
     console.log("Server listening at port 3000");
